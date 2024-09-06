faa6a0c870c05debb8bfa695e7f3e1dbaf4c7d35
abrahem haj ali <boss.abrahem@hotmail.com>
2024-09-06 14:30:00 +0300

    added modal to the product image when clicked

--- index.js
+++ index.js
@@ -12,3 +12,34 @@ otherImages.forEach((image) => {
     zoomedImage.src = image.src;
   });
 });
+
+const template = document.querySelector("#modalTemplate");
+
+mainImage.addEventListener("click", () => {
+  const modalClone = template.content.cloneNode(true);
+  document.body.appendChild(modalClone);
+
+  const modal = document.querySelector(".modal");
+  const modalImage = document.querySelector(".modal-content img");
+
+  modalImage.src = mainImage.src;
+  modal.style.display = "flex";
+  modal.style.overflow = "hidden";
+
+  window.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+      if (document.body.contains(modalClone)) {
+        document.body.removeChild(modalClone);
+      }
+    }
+  });
+
+  const modalCloseButton = document.querySelector(".close");
+  modalCloseButton.addEventListener("click", () => {
+    modal.style.display = "none";
+    if (document.body.contains(modalClone)) {
+      document.body.removeChild(modalClone);
+    }
+  });
+});
